2f8ca1ef5a04770328eaee3e3d99d54d2679be18
akshcs <akash2103.singh@gmail.com>
2022-12-20 02:34:32 +0530

    Add Properties

---
+++ src/main/java/com/example/deploy/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.deploy.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Hello/")
+public class DemoController {
+
+    @GetMapping("")
+    String home() {
+        return "Hello Folks Welcome to Demo Deploy";
+    }
+}
